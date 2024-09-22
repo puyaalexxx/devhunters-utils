cf38e4aedc601d29207bfa89fe86619538f63b60
Alex <puyaalexxx@gmail.com>
2024-09-22 22:46:09 +0300

    added d.ts file for utils.ts file

---
+++ dts-files/utils.d.ts
@@ -0,0 +1,5 @@
+// Re-export everything from your utility functions
+export * from './utils/dynamic-module-loading';
+export * from './utils/file-handlers';
+export * from './utils/ts-file-names';
+export * from './utils/vite-configs';
